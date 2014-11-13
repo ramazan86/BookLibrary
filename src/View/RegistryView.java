038d05292b82b2bb001919aa58d6facd041104ea
Ramazan Cinardere <ramazanc86@hotmail.com>
2014-11-13 17:33:22 +0100

    Ramazan: User, Update names of classes in View Package

--- src/View/RegistryView.java
+++ src/View/RegistryView.java
@@ -9,12 +9,12 @@ package View;
  *
  * @author ibm
  */
-public class Registry extends javax.swing.JFrame {
+public class RegistryView extends javax.swing.JFrame {
 
     /**
      * Creates new form Registry
      */
-    public Registry() {
+    public RegistryView() {
         initComponents();
     }
 
@@ -207,20 +207,21 @@ public class Registry extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Registry.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistryView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Registry.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistryView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Registry.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistryView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Registry.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistryView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Registry().setVisible(true);
+                new RegistryView().setVisible(true);
             }
         });
     }
